7da4c2478f4a756c26a70f7b5c09d2c4974b651a
EDWARD-125 <edariopinto@unicesar.edu.co>
2025-10-17 20:05:46 -0500

    problema del tests resuelto

---
+++ backend/src/test/java/com/carcafe/multicloud/MulticloudApplicationTests.java
@@ -0,0 +1,12 @@
+package com.carcafe.multicloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = VmProvisioningApiApplication.class)
+class MulticloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
